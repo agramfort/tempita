2625d9c481c1925d4365c10cc298fc686ba6c8da
ianb <devnull@localhost>
2007-07-24 01:19:20 +0000

    docstring updates

--- setup.py
+++ setup.py
@@ -13,6 +13,13 @@ This isn't meant to be the Next Big Thing in templating; it's just a
 handy little templating language for when your project outgrows
 ``string.Template`` or ``%`` substitution.  It's small, it embeds
 Python in strings, and it doesn't do much else.
+
+You can read about the `language
+<file:///home/ianb/src/Tempita/docs/html/#the-language>`_, the
+`interface <file:///home/ianb/src/Tempita/docs/html/#the-interface>`_,
+and there's nothing more to learn about it.
+
+0.1 is the initial release.
 """,
       classifiers=[
         'Development Status :: 4 - Beta',
@@ -28,8 +35,4 @@ Python in strings, and it doesn't do much else.
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=True,
-      install_requires=[
-      ],
-      entry_points="""
-      """,
       )
